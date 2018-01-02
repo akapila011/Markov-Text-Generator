2668536a9a287c0662feb5e1aed35ab911afeeea
AKapila <akapila011@gmail.com>
2018-01-02 23:11:30 +0300

    Finished designing gui, basic markov model structure created

--- Markov Chain Sentence Generator/MainWindow.xaml.cs
+++ Markov Chain Sentence Generator/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Markov_Chain_Sentence_Generator
 {
@@ -20,9 +22,81 @@ namespace Markov_Chain_Sentence_Generator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Markov mk = null;
+        String text = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDailog = new OpenFileDialog();
+            openFileDailog.FileName = ""; // Default file name
+            openFileDailog.DefaultExt = ".txt"; // Default file extension
+            openFileDailog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+            Nullable<bool> result = openFileDailog.ShowDialog();
+            if (result == true)
+            {
+                // Open document
+                string filepath = openFileDailog.FileName;
+                try
+                {
+                    text = File.ReadAllText(filepath);
+                    filepath_textBox.Text = filepath;
+                }
+                catch (IOException ioEx)
+                {
+                    MessageBox.Show("There was a problem reading the slected file. Error: " + ioEx.Message);
+                    text = null;
+                    filepath_textBox.Text = "";
+                }
+            }
+        }   // end SelectFile()
+
+        private void TrainModel(object sender, RoutedEventArgs e)
+        {
+            if (text == null)
+            {
+                MessageBox.Show("There is no text available for training.");
+                return;
+            }
+            mk = new Markov("Test", text);
+            try
+            {
+                mk.Train();
+                MessageBox.Show("Successfully Trained Markov model. You can now generate sentences.");
+                trainingStatus_label.Content = "Trained";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                trainingStatus_label.Content = "Not Trained";
+            }
+        }
+
+        private void GenerateText(object sender, RoutedEventArgs e)
+        {
+            if (mk == null)
+            {
+                MessageBox.Show("You must first select a text file and click train before generating text.");
+                return;
+            }
+            if ((String)trainingStatus_label.Content != "Trained")
+            {
+                MessageBox.Show("There was a problem during loading and training the model. Please try again.");
+                return;
+            }
+            try
+            {
+                generatedText_textBlock.Text = mk.GenerateSentence();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem with generating text. Error: " + ex.Message);
+                generatedText_textBlock.Text = "";
+            }
         }
     }
 }
--- Markov Chain Sentence Generator/Markov.cs
+++ Markov Chain Sentence Generator/Markov.cs
@@ -13,11 +13,14 @@ namespace Markov_Chain_Sentence_Generator
         private Dictionary<String, List<String>> model;
         private bool trained;
 
+        static Random random = new Random();
+
 
         public Markov(String name, String text)
         {
             this.ModelName = name;
             this.text = text;
+            model = new Dictionary<string, List<string>>();
             model["START"] = new List<string>();
             model["END"] = new List<string>();
             this.trained = false;
@@ -27,6 +30,10 @@ namespace Markov_Chain_Sentence_Generator
         {
             trained = false;
             String[] sentences = text.Split('.');
+            if (sentences.Length < 2)
+            {
+                throw new FormatException("The text provided has no periods('.'). Please use a text with sentences seperated with periods.");
+            }
             for(int i=0; i!=sentences.Length; i++)
             {
                 String[] words = sentences[i].Split(' ');
@@ -34,24 +41,60 @@ namespace Markov_Chain_Sentence_Generator
                 for(int j=0; j!=words.Length; j++)
                 {
                     String word = words[j].ToLower();
-                    if (j == 0)  // first word, add to start
-                    {
-                        model["START"].Add(word);
-                    }
-                    else if (j == words.Length -1)  // last word, goes to end
+                    if (j == words.Length - 1)  // last word, goes to end
                     {
                         model["END"].Add(word);
                     }
-                    // Now create an entry for each word if it doesn't exist
+                    else   // end words cannot be saved as indexes for lookup in the model, only start and middle words
+                    {  
+                        if (j == 0)  // first word, add to start
+                        {
+                            model["START"].Add(word);
+                        }
+                        model[word] = new List<string>();  // create entry for current word and add the preceding word to its list
+                        model[word].Add(words[j + 1].ToLower());
+                    }
                 }
             }
-
-
         }   // end Train()
 
         public String GenerateSentence()
         {
-            return "";
+            List<String> sentence = new List<string>();
+            while (true)
+            {
+                if (sentence.Count == 0) // no words used yet
+                {
+                    sentence.Add(model["START"][random.Next(model["START"].Count)]); // choose any random word from list of start words
+                }
+                else if (IsEndWord(sentence[sentence.Count-1]))  // last word, stop generating text
+                {
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        String lastWord = sentence[sentence.Count - 1];
+                        sentence.Add(model[lastWord][random.Next(model[lastWord].Count)]);
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        continue;
+                    }
+                }
+            }
+            return String.Join(" ", sentence);
         }   // end GenerateSentence()
+
+        private bool IsEndWord(String word)
+        {
+            foreach(String w in model["END"])
+            {
+                if (w == word) { return true; }  // match found
+            }
+            return false;
+        }   // end IsEndWord()
+
     } // end Markov{}
 }
